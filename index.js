8bf3d07e4aa901c86c30a8cf315a93f6ed5372ae
Rohit Patil <rohit3.patil@ril.com>
2021-08-08 21:22:29 +0530

    server setup

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.json({"message": "Welcome to HomeLand query service"});
 });
 
 app.listen(process.env.PORT || 5000);
